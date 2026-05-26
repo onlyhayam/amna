63b2aa3ce6c7b47a8fcb9c386c03a1341b153541
onlyhayam <github@only.hayam>
2026-05-27 00:20:54 +0530

    Sample JS added. Tested on home page and Account page

---
+++ web/main.js
@@ -0,0 +1,5 @@
+const homeName = document.getElementById("name");
+homeName.textContent = "Hayam!";
+
+const pageTitle = document.getElementById("account");
+pageTitle.innerHTML = `<img src=/img/account.svg alt=account> ACCOUNT  <a class="mode" id="theme-toggle"> </a>`;
--- web/mode.js
+++ web/mode.js
@@ -2,4 +2,5 @@ const toggle = document.getElementById("theme-toggle");
 
 toggle.addEventListener("click", () => {
   document.documentElement.classList.toggle("light");
+  document.documentElement.classList.toggle("dark");
 });
