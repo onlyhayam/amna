acc3ea26effc601e9d85b3b173bd3919b3802c6e
onlyhayam <github@only.hayam>
2026-05-29 16:58:26 +0530

    input field added for home

--- web/script/mode.js
+++ web/script/mode.js
@@ -1,6 +1,15 @@
 const toggle = document.getElementById("theme-toggle");
 
+const savedTheme = sessionStorage.getItem("theme") || "dark";
+document.documentElement.classList.add(savedTheme);
+
 toggle.addEventListener("click", () => {
-  document.documentElement.classList.toggle("light");
-  document.documentElement.classList.toggle("dark");
+  const isDark = document.documentElement.classList.contains("dark");
+  const newTheme = isDark ? "light" : "dark";
+
+  document.documentElement.classList.replace(
+    isDark ? "dark" : "light",
+    newTheme,
+  );
+  sessionStorage.setItem("theme", newTheme);
 });
